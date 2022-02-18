1a2921e20c3c85e99f7abb9995903da27feb1658
winZD <puntica007@gmail.com>
2022-02-18 15:08:53 +0100

    finished outh flow

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+//keys.js figure out what set of credentials to return
+
+if (process.env.NODE_ENV === "production") {
+  //return the prod set of keys
+  module.exports = require("./prod");
+} else {
+  //return the dev set of keys
+  module.exports = require("./dev");
+}
