856777d2025c2913db6806697bcd41da6bec5196
winZD <puntica007@gmail.com>
2022-02-18 17:33:11 +0100

    fixing issue

--- config/keys.js
+++ config/keys.js
@@ -5,5 +5,5 @@ if (process.env.NODE_ENV === "production") {
   module.exports = require("./prod");
 } else {
   //return the dev set of keys
-  module.exports = require("./dev");
+  module.exports = require("./dev"); //
 }
