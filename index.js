2a03811159e31c3728113218a1b259e3a4770198
winZD <puntica007@gmail.com>
2022-03-05 16:17:42 +0100

    fix 7

--- index.js
+++ index.js
@@ -5,12 +5,7 @@ const passport = require("passport");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 //
-const module = require("module");
-const fs = require("fs");
-module.Module._extensions[".js"] = function (module, filename) {
-  const contents = fs.readFileSync(filename, "utf8");
-  module._compile(require("fs").readFileSync(filename, "utf8"), filename);
-};
+
 //
 require("./models/User");
 require("./services/passport");
